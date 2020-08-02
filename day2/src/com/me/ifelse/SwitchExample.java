23ee1ade833012366ffcdd2b448db9d57286a696
Ikhlas Ahmed <ike.ahmed@aa.com>
2020-08-02 09:18:32 -0500

    updates

---
+++ day2/src/com/me/ifelse/SwitchExample.java
@@ -0,0 +1,19 @@
+package com.me.ifelse;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        int value = 1;
+        switch (value) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.print("Value was not 1 or 2");
+                break;
+        }
+    }
+
+}
